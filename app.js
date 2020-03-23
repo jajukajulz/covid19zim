90119ba1e173448bb4d913d5cb6b6a75b2769e8c
juliank <julian.kanjere@jskdezign.com>
2020-03-24 01:50:59 +0200

    Update server

--- app.js
+++ app.js
@@ -3,15 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var ejs = require('ejs');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+// expose bootstrap
+app.use('/scripts/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/')));
+app.use('/scripts/jquery', express.static(path.join(__dirname, 'node_modules/jquery/dist/')));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
