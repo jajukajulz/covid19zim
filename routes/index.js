09fce9f9c0cb92aad8b9c860ea6ebc350bb2090f
juliank <julian.kanjere@jskdezign.com>
2020-03-24 01:52:05 +0200

    Update routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var title = 'covid19zim Dashboard. Near-real time statistics and information about the COVID-19 in Zimbabwe.';
+  var title_main = 'covid19zim Dashboard.';
+  var title_sub = 'Near-real time statistics and information about the COVID-19 in Zimbabwe.';
+  res.render('index', { title: title, title_main: title_main, title_sub: title_sub  });
 });
 
 module.exports = router;
