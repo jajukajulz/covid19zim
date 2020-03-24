1377d39b27e056aa0a90710fe70b80b6e420c697
juliank <julian.kanjere@jskdezign.com>
2020-03-24 02:22:03 +0200

    Working site

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var title = 'covid19zim Dashboard. Near-real time statistics and information about the COVID-19 in Zimbabwe.';
-  var title_main = 'covid19zim Dashboard.';
+  var title = 'Covid19Zim | Near-real time statistics and information about the COVID-19 in Zimbabwe.';
   var title_sub = 'Near-real time statistics and information about the COVID-19 in Zimbabwe.';
-  res.render('index', { title: title, title_main: title_main, title_sub: title_sub  });
+  var title_hastags = '#Covid19Zim #FlattenTheCurve #StayHomeSaveLives';
+  var title_main = 'Coronavirus COVID-19 Cases in Zimbabwe.';
+  res.render('index', { title: title, title_main: title_main, title_sub: title_sub, title_hastags: title_hastags  });
 });
 
 module.exports = router;
