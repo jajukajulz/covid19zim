23031dafc4938fd4671b9bf61da2ce0cff8005f4
juliank <julian.kanjere@jskdezign.com>
2020-03-29 02:49:41 +0200

    add redis cache

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var ejs = require('ejs');
 var sslRedirect = require('heroku-ssl-redirect');
 
+// Load Firebase Credentials
 const admin = require('firebase-admin');
 let serviceAccount = require('./serviceAccountKey.json');
 
@@ -14,23 +15,36 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
+// Create Friebase db handle
 let db = admin.firestore();
 
+// Load Express Framework
 var app = express();
+
+// Load Redis cache. Cache entries don't expire unless otherwise specified.
+// e.g. 60 seconds var cache = require('express-redis-cache')({ expire: 60 });
+var cache = require('express-redis-cache')();
 
 app.use(bodyParser.json()); //need to parse HTTP request body
 
-//1 collection has 0 or more documents
+// 1 collection has 0 or more documents
+let docRef = db.collection('stats').doc('consts1');
 
+let setAda = docRef.set({
+  first: 'Ada',
+  last: 'Lovelace',
+  born: 1815
+});
 
+// Import Index router
 var indexRouter = require('./routes/index');
 
 
-// expose bootstrap
+// Expose bootstrap
 app.use('/scripts/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/')));
 app.use('/scripts/jquery', express.static(path.join(__dirname, 'node_modules/jquery/dist/')));
 
-// enable ssl redirect
+// Enable ssl redirect
 app.use(sslRedirect([
   'other',
   'development',
@@ -65,5 +79,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,66 @@
 const admin = require('firebase-admin');
 var express = require('express');
+
 var router = express.Router();
+var cache = require('express-redis-cache')();
+
+
 let db = admin.firestore();
 
+const SECONDS_IN_A_DAY = 86400;
+const SECONDS_1000 = 1000;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  var title = 'Covid19Zim Aggregator | Near-real time statistics and information about the COVID-19 in Zimbabwe.';
-  var title_sub = 'Near-real time statistics and information about the COVID-19 in Zimbabwe.';
-  var title_hastags = '#Covid19Zim #FlattenTheCurve #StayHomeSaveLives';
-  var title_main = 'Coronavirus COVID-19 Cases in Zimbabwe.';
-  res.render('index', { title: title, title_main: title_main, title_sub: title_sub, title_hastags: title_hastags  });
+router.get('/', cache.route({
+    expire: {
+    200: SECONDS_IN_A_DAY,
+    404: 10,
+    403: 5000,
+    500: 10,
+    xxx: 1
+  }
+}), (req, res, next) => {
+    var title = 'Covid19Zim Aggregator | Near-real time statistics and information about the COVID-19 in Zimbabwe.';
+    var title_sub = 'Near-real time statistics and information about the COVID-19 in Zimbabwe.';
+    var title_hastags = '#Covid19Zim #FlattenTheCurve #StayHomeSaveLives';
+    var title_main = 'Coronavirus COVID-19 Cases in Zimbabwe.';
+    res.render('index', { title: title, title_main: title_main, title_sub: title_sub, title_hastags: title_hastags,
+  });
 });
+
+router.get('/caches/view', function(req, res, next){
+  console.log("View caches");
+  cache.get(function (error, entries) {
+    if ( error ) throw error;
+  
+    entries.forEach(console.log.bind(console));
+  });
+});
+
+router.get('/caches/delete', function(req, res, next){
+  console.log("Flush caches");
+  cache.del( '*', next);
+  });
+
+
+router.get('/caches/size', function(req, res, next){
+    console.log("View cache size");
+    cache.size( );
+    });
+
+// Serve simple page with timestamp cached for 5 seconds
+router.get('/caches/test/:skip_cache?',
+  cache.route({ expire: 10000  }), // cache entry will live 10000 seconds
+  function (req, res)  {
+    console.log ("Test 10 second cache. If you add skip_cache param then cache disabled");
+  	if (req.params.skip_cache) {
+  		res.use_express_redis_cache = false;
+  		console.log ("Cache disabled on this request");
+  	}
+
+    var currTime = new Date();
+    res.send("Date and time: " + currTime);
+});    
 
 //Add
 // let docRef = db.collection('stats').doc('consts1');
