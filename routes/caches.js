1266e70f3d8a4e482b0142352170e03447705a71
juliank <julian.kanjere@jskdezign.com>
2020-04-06 01:57:27 +0200

    Add auth to cache routes

--- routes/caches.js
+++ routes/caches.js
@@ -7,35 +7,36 @@ const utils = require('./../utils');
 
 
 // Check cache keys
-router.get('/caches/view', function(req, res, next){
+router.get('/caches/view', ensureLoggedIn(), function(req, res, next){
   console.log("View caches");
   var cache_keys = mcache.keys();
   res.send(`Cache keys - ${cache_keys}`);
   });
 
 // Check cache json
-router.get('/caches/json', function(req, res, next){
+router.get('/caches/json', ensureLoggedIn(), function(req, res, next){
   console.log("View caches json");
   res.json(mcache.exportJson());
   });
 
 // Flush cache
-router.get('/caches/delete', function(req, res, next){
+router.get('/caches/delete', ensureLoggedIn(), function(req, res, next){
   console.log("Flush caches");
   mcache.clear();
   res.send("Cache cleared");
   });
 
 // Get cache size
-router.get('/caches/size', function(req, res, next){
+router.get('/caches/size', ensureLoggedIn(), function(req, res, next){
     console.log("View cache size");
     var cache_size = mcache.size();
     res.send(`Cache size - ${cache_size}`);
     });
 
 // Serve simple page with timestamp cached for 5 seconds
-router.get('/caches/test/',
-  utils.cache.cacheMiddleware(utils.cache.SECONDS_10), // cache entry will live 10000 seconds
+router.get('/caches/test/', [
+    ensureLoggedIn(),
+    utils.cache.cacheMiddleware(utils.cache.SECONDS_10)], // cache entry will live 10000 seconds
   function (req, res)  {
     console.log ("Test 10 second cache.");
 
